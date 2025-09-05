efecdf2983235900be44e28ad0a7607a2a803d54
pravin-kavthale <kavthalepravin@gmail.com>
2025-09-05 22:28:37 +0530

    Update URL patterns to change root path to login and add Events path for event URLs

--- EventHub/urls.py
+++ EventHub/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',include('Event.urls')),
-    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('Events/',include('Event.urls')),
+    path('',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
 ]
