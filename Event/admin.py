05d25abd1479cda0f6ebddd6eb9fb9cd775ed638
pravin-kavthale <kavthalepravin@gmail.com>
2025-09-07 13:29:49 +0530

    Add Event model with relationships and image field; update Profile model for image resizing

--- Event/admin.py
+++ Event/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'start_time', 'end_time', 'created_by')
+    list_filter = ('category', 'start_time')
+    search_fields = ('title', 'location', 'description')
--- Event/models.py
+++ Event/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from djnago.contrib.authm.models import user
 
 # Create your models here.
+
+class Event(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('music', 'Music'),
+        ('art', 'Art'),
+        ('sports', 'Sports'),
+        ('tech', 'Tech'),
+        ('travel', 'Travel'),
+        ('other', 'Other'),
+    ]
+
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    category=models.CharField(max_length=100,choices=CATEGORY_CHOICES, default='other')
+    date=models.Date()
+    Location=models.CharField(max_length=300)
+    start_time=models.TimeField()
+    end_time=models.TimeField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+    #relationship
+    organizer=models.ForeignKey(User,on_delete=models.CASCADE,related_name='events_creater')
+    participants=models.ManyToManyField(User,related_name='events_participated',blank=True)
+
+     # optional image
+    image = models.ImageField(upload_to="event_images/", default="event_images/default.png")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-start_time'] 
+
