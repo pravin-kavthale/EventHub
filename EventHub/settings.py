9d51b1609cf426d459a5417ffe4006004f953db9
pravin-kavthale <kavthalepravin@gmail.com>
2025-12-30 13:57:25 +0530

    Refactor database configuration to remove default values for DB_NAME, DB_USER, and DB_HOST

--- EventHub/settings.py
+++ EventHub/settings.py
@@ -87,14 +87,13 @@ WSGI_APPLICATION = "EventHub.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME", default="eventhub"),
-        "USER": config("DB_USER", default="postgres"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST", default="localhost"),
+        "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT", default="5432"),
     }
 }
-
 # --------------------
 # PASSWORD VALIDATION
 # --------------------
